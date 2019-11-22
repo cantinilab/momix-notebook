520933af0eedb91ab1dfc073346176d884bc37e4
Celine Hernandez <celine.hernandez@curie.fr>
2019-11-22 16:01:06 +0100

    Add comments describing what the code is doing.

--- scripts/log2matrix.R
+++ scripts/log2matrix.R
@@ -1,7 +1,13 @@
-log2matrix <- function(folder,file.name){
-  data<-as.matrix(read.table(paste(folder,file.name,sep="/"),sep=" ",row.names=1,header=T))
-  data<-log2(data+1)
-  output<-paste(folder,paste("log_",file.name,sep=""),sep="/")
-  write.table(data,output,sep=" ", col.names=T, row.names=T)
-  system(paste("sed -i '1s/^/probe\t/'", output, sep=" "))
+## Support function to apply log2(+1) to a matrix
+log2matrix <- function(folder, file.name){
+    # Read table
+    data <- as.matrix(read.table(paste(folder,file.name,sep="/"),sep=" ",row.names=1,header=TRUE))
+    # Apply transformation
+    data <- log2(data+1)
+    # Output file name
+    output <- paste(folder,paste0("log_",file.name), sep="/")
+    # Export transformed data
+    write.table(data,output,sep=" ", col.names=TRUE, row.names=TRUE)
+    # ?
+    system(paste("sed -i '1s/^/probe\t/'", output, sep=" "))
 }
